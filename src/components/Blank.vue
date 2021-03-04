7b47002ae711514293a6a3c13631dfadce54b0d1
Eric Yang <IamSpazzy@gmail.com>
2021-03-04 07:04:04 -0500

    added components

---
+++ src/components/Blank.vue
@@ -0,0 +1,38 @@
+<template>
+  <div class="content">
+    <div class="vertical-center">
+      <button>Centered Button</button>
+    </div>
+  </div>
+</template>
+
+<script>
+
+export default {
+  name: 'Blank',
+  props: {
+    parentid: String,
+    msg: String
+  },
+  mounted() {
+    console.log('boo');
+  }
+
+
+}
+</script>
+
+<style scoped>
+.content {
+  display: flex;
+  align-items: stretch; 
+  height: 100%;
+}
+.vertical-center {
+  margin: 0;
+  position: absolute;
+  top: 50%;
+  left: 50%;
+  transform: translate(-50%, -50%)
+}
+</style>
